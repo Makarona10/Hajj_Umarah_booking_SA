7477584ddc61f42d84bb5dd5cf8a3d2f60ae3086
Makarona10 <ahmedneyssi@gmail.com>
2024-04-29 18:21:39 +0300

    Transportation's partially over

--- Backend/Transportation/routes/Transport.routes.js
+++ Backend/Transportation/routes/Transport.routes.js
@@ -1,21 +1,28 @@
 const controller = require("../controllers/controller")
 const express = require("express")
+const morgan = require("morgan")
 const {body_valid} = require("../middleware/middleware")
 const app = express()
 const route = express.Router()
 
 
 app.use(express.json())
+app.use(morgan('dev'))
 app.use('/transport', route)
 
 route.route('/')
     .get(controller.list_trans)
     .post(body_valid ,controller.reserve)
 
-    route.route('/api/modify')
+route.route('/modify/:sort')
     .patch(body_valid, controller.edit_avail)
+    
+route.route('/units')    
+    .post(body_valid, controller.add_rem_units)
 
 
 app.listen(4001, () => {
     console.log("Listening on port: 4001");
 })
+
+module.exports = route;
