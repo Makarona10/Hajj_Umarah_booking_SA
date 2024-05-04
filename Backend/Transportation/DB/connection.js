fb8096dc766894df8f1c8c222349f319bedf16cc
Makarona10 <ahmedneyssi@gmail.com>
2024-05-04 20:23:14 +0300

    Trans docker is over

--- Backend/Transportation/DB/connection.js
+++ Backend/Transportation/DB/connection.js
@@ -5,19 +5,28 @@ evar.config()
 // console.log(process.env.HOST, process.env.DB_USER, process.env.DB_NAME)
 const connect = sql.createPool({
     host: 'trans-DB',
-    user: 'users',
+    user: 'root',
+    password: '1234',
     database: 'trans',
     port: 3306,
 }).promise()
 
 
 connect.query(`CREATE TABLE IF NOT EXISTS inventory (
-    Type VARCHAR(20),
-    units INT,
-    avail_units INT,
-    seats INT,
-    free_seats INT,
+    Type VARCHAR(20) PRIMARY KEY,
+    units BIGINT,
+    avail_units BIGINT,
+    seats BIGINT,
+    free_seats BIGINT
 )`).then(() => console.log("CREATED!!")).catch((error) => console.log(error));
+
+connect.query(`INSERT INTO inventory (Type, units, avail_units, seats, free_seats)
+                VALUES ("Ground", 0, 0, 0, 0),
+                ("AIR", 0, 0, 0, 0),
+                ("Nautical voy", 0, 0, 0, 0)
+                `).then(() => consolele.log("Inserted!"))
+                .catch(() => console.log("This is not the first time here!"));
+
 // const res = connect.query('INSERT INTO inventory (Type ,units, avail_units, seats, free_seats) VALUES ("Ground", 0, 0, 0, 0)');
 // connect.query(`SELECT * FROM inventory`)
 //   .then(res => {
--- Backend/Transportation/routes/Transport.routes.js
+++ Backend/Transportation/routes/Transport.routes.js
@@ -1,26 +1,14 @@
 const controller = require("../controllers/controller")
 const express = require("express")
 const morgan = require("morgan")
-const {body_valid} = require("../middleware/middleware")
 const app = express()
 const route = express.Router()
-
-
-app.use(express.json())const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
-
-const controller = require("../controllers/controller");
 const { body_valid } = require("../middleware/middleware");
-
-const app = express();
-const route = express.Router();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
-app.use("/transport", route);
 
 route.route("/")
     .get(controller.list_trans)
@@ -45,12 +33,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(4001, () => {
-    console.log("Listening on port: 4001");
-});
-
-module.exports = route;
-app.use(morgan('dev'))
 app.use('/transport', route)
 
 route.route('/')
