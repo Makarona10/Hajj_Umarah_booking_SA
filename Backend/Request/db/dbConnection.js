495f229f41f67dd4dd601c2c02560f1e33d5efce
Makarona10 <ahmedneyssi@gmail.com>
2024-05-07 14:41:36 +0300

    All services are nearly over

--- Backend/Request/db/dbConnection.js
+++ Backend/Request/db/dbConnection.js
@@ -8,18 +8,18 @@ const connection = mysql.createPool({
 }).promise();
 
 connection.query(`CREATE TABLE appointment_requests (
-  id int NOT NULL PRIMARY KEY,
-  appointment_id int NOT NULL,
-  traveler_id int NOT NULL,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  appointment_id VARCHAR(255) NOT NULL,
+  traveler_id VARCHAR(255) NOT NULL,
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   type varchar(255) NOT NULL
 )`).then(() => console.log("Welcome for the first time .. !")).catch(() => console.log("Welcome back!"))
-connection.query(`CREATE TABLE IF NOT EXISTS users (
-  id int(11) NOT NULL,
-  username int(11) NOT NULL
-)`)
 
-connection.query(`SHOW TABLES`).then((res) => console.log(res)).catch((err) => console.log(err));
+// connection.query(`CREATE TABLE IF NOT EXISTS users (
+//   id int(11) NOT NULL,
+//   username int(11) NOT NULL
+// )`)
+
 
 
 
